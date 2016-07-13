d852c3e648494ed4fb22ea317c52054050e3c8c4
Bozakov <zdravko.bozakov@emc.com>
2016-07-13 23:51:21 +0100

    refactored CIB sources

--- policy/cib.py
+++ policy/cib.py
@@ -2,382 +2,162 @@ import logging
 import os
 import operator
 import json
+import bisect
+import itertools
 
 from collections import ChainMap
 
-from policy import  NEATRequest, NEATCandidate, NEATProperty, PropertyDict, NEATPropertyError
+import shutil
+
+from policy import NEATRequest, NEATCandidate, NEATProperty, PropertyArray, PropertyMultiArray, NEATPropertyError
+from policy import dict_to_properties
 
 logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.DEBUG)
 
-LOCAL = 'local'
-CONNECTION = 'connection'
-REMOTE = 'remote'
-ENTRIES = 'entries'
+
+class CIBSource(object):
+    cib = None
+
+    def __init__(self, source_dict):
+        self.idx = source_dict['id']
+        self.root = source_dict.get('root', False)
+        self.priority = source_dict.get('priority', 0)
+        self.filename = source_dict.get('filename', None)
+        self.description = source_dict.get('description', '')
+
+        self.properties = PropertyMultiArray(*dict_to_properties(source_dict.get('properties')))
+        self.refs = set(source_dict.get('@next', []))
+
+    def resolve_refs(self, path=None):
+        if path is None:
+            path = []
+        # insert own index based on CIB source priority to resolve overlapping properties later
+        pos = bisect.bisect([self.cib[idx].priority for idx in path], self.priority)
+        path.insert(pos, self.idx)
+
+        # no more references to check
+        if not (self.refs - set(path)):
+            return [path]
+
+        new_paths = []
+        for idx in self.refs:
+            if idx in path:
+                continue
+            new_paths.extend(self.cib[idx].resolve_refs(path.copy()))
+        return new_paths
+
+    def expand(self):
+        paths = self.resolve_refs()
+        properties = []
+        for p in paths:
+            expanded_properties = (self.cib[idx].properties.expand() for idx in p)
+            for i in itertools.product(*expanded_properties):
+                # print(i)
+                properties.append(ChainMap(*i))
+        return properties
+
+    def __repr__(self):
+        s = ''
+        return "%s @refs%s" % (self.properties, self.refs)
+
+
+def load_json(filename):
+    """Read CIB source from JSON file"""
+
+    cib_file = open(filename, 'r')
+    try:
+        j = json.load(cib_file)
+    except json.decoder.JSONDecodeError as e:
+        logging.error("Could not parse CIB file " + filename)
+        print(e)
+        return
+    return j
 
 
 class CIB(object):
-    """Internal representation of the CIB for testing"""
+    """
+    Internal representation of the CIB for testing
 
-    # We don't care about some of the CIB properties for matching. Define these special keys here
-    meta_keys = {'id', 'type', 'remote', 'local', 'filename', 'cib_source', 'timestamp', 'connection'}
+    """
 
     def __init__(self, cib_dir=None):
-
-        # define three repository types for CIB sources
-        self.local = {}
-        self.connection = {}
-        self.remote = {}  # TODO rename remote to destination?
-
-        # concatenate all CIB repositories
-        self.entries = ChainMap(self.remote, self.connection, self.local)
+        self.idx = {}
+        CIBSource.cib = self
 
         if cib_dir:
             self.load_cib(cib_dir)
 
-    def __getitem__(self, idx):
-        """Return a new PropertyDict containing the properties associated with a CIB index"""
-        entry = self.entries[idx]
-        properties = PropertyDict()
-
-        if not entry.get('type') == CONNECTION:
-            for k, v in entry.items():
-                if isinstance(v, NEATProperty):
-                    properties.insert(v)
-
-        else:
-            # for connection type entries expand the associated local and remote entries
-            local_id = entry.get('local')
-            remote_id = entry.get('remote')
-
-            new_entry = {}
-
-            # properties are overwritten in the following order: remote>connection>local
-            new_entry.update({i.key: i for i in self.entries[local_id].values() if isinstance(i, NEATProperty)})
-            new_entry.update({i.key: i for i in self.entries[idx].values() if isinstance(i, NEATProperty)})
-            new_entry.update({i.key: i for i in self.entries[remote_id].values() if isinstance(i, NEATProperty)})
-
-            for k, v in new_entry.items():
-                properties.insert(v)
-
-        return properties
-
-    def load_json(self, filename):
-        """Read JSON file"""
-
-        cib_file = open(filename, 'r')
-        try:
-            j = json.load(cib_file)
-        except json.decoder.JSONDecodeError as e:
-            logging.error("Could not parse CIB file " + filename)
-            print(e)
-            return
-        return j
+    @property
+    def roots(self):
+        return {k: v for k, v in cib.idx.items() if v.root}
 
     def load_cib(self, cib_dir='cib/'):
         """Read all CIB source files from directory CIB_DIR"""
         for filename in os.listdir(cib_dir):
-            # TODO fix order of loading CIB sources
-            if filename.endswith(('.local', '.remote', '.connection')):
+            if filename.endswith(('.local', '.cib', '.connection')) and not filename.startswith(('.', '#')):
                 print('loading CIB source %s' % filename)
-                p = self.load_json(cib_dir + filename)
-                if not p:
+                cs = load_json(cib_dir + '/' + filename)
+                if not cs:
                     continue
-                p['filename'] = filename
-                properties = PropertyDict()
-                properties.insert_dict(p.get('properties', {}))
-                # TODO convert CIB to PropertyDict
-                # convert JSON properties to NEATProperties
-                for key, value in p.pop('informational', {}).items():
-                    p[key] = NEATProperty((key, value), precedence=NEATProperty.INFORMATIONAL)
-                for key, value in p.pop('requested', {}).items():
-                    p[key] = NEATProperty((key, value), precedence=NEATProperty.REQUESTED)
-                for key, value in p.pop('immutable', {}).items():
-                    p[key] = NEATProperty((key, value), precedence=NEATProperty.IMMUTABLE)
+                cib_source = CIBSource(cs)
+                cib_source.filename = filename
 
-                self.register(p)
+                self.register(cib_source)
 
-    def register(self, cib_source={}):
-        """Register loaded CIB sources with CIB repository"""
-        # CIB types [local] <<>> [connection] <<>> [remote]
-        current_type = cib_source.get("type")
-        if current_type not in ['local', 'connection', 'remote']:
-            logging.warning("Ignoring invalid type \"" + current_type + "\" for CIB " + cib_source['filename'])
-            return
-        current_idx = cib_source.get("id")
-        # cib_properties = PropertyDict()
-        # TODO for now we assume that all CIB properties are informational
-        # FIXME
-        # cib_properties.update(cib_source, precedence=NEATProperty.INFORMATIONAL)
-
-        # e.g. self.local['d1'] = {...}
-        getattr(self, current_type)[current_idx] = cib_source
-
-    def get_connection(self, idx):
-        """Return the full connection properties including the properties of the
-associated local and remote CIB entries.
-
-        """
-        connection = {}
-        connection.update(self.connection[idx])
-        remote_idx = connection[REMOTE]
-        local_idx = connection[LOCAL]
-        connection.update(self.local[local_idx])
-        connection.update(self.remote[remote_idx])
-        return {k: connection[k] for k in connection.keys() - CIB.meta_keys}
-
-    def lookup(self, query, candidate_num=5):
-        """CIB lookup logic implementation. Appends a list of connection candidates to the query object. TODO
-        """
-        candidates = []
-
-        # check connection CIB sources first
-        for idx in self.connection.keys():
-            matched_properties = self[idx] & query.properties
-            candidate = NEATCandidate(self[idx])
-            skip_candidate = False
-            for property in matched_properties.values():
-                try:
-                    candidate.properties.insert(property)
-                except NEATPropertyError as e:
-                    logging.debug(e)
-                    skip_candidate = True
-                    break
-            if skip_candidate:
-                continue
-            candidates.append(candidate)
-
-        candidates.sort(key=operator.attrgetter('score'), reverse=True)
-        query.candidates = candidates[0:candidate_num]
-        # TODO expand lookup to different cib types
-
-        for idx in self.local.keys():
-            matched_properties = self[idx] & query.properties
-            candidate = NEATCandidate(self[idx])
-            skip_candidate = False
-            for property in query.properties.values():
-                try:
-                    candidate.properties.insert(property)
-                except NEATPropertyError as e:
-                    logging.debug(e)
-                    skip_candidate = True
-                    break
-            if skip_candidate:
-                continue
-            candidates.append(candidate)
-        candidates.sort(key=operator.attrgetter('score'), reverse=True)
-        query.candidates = candidates[0:candidate_num]
-
-    def dump(self):
-        keys = list(self.entries.keys())
-        keys.sort()
-        for k in keys:
-            print('%s: %s' % (k, self[k]))
-
-    def __repr__(self):
-        return 'CIB<%d>' % (len(self.local) + len(self.connection) + len(self.remote))
-
-    def __str__(self):
-        s = ''
-        for k, v in self.connection.items():
-            if not k == 'type':
-                s += str(v[LOCAL]) + ' <-> ' + str(k) + ' <-> ' + str(v[REMOTE]) + '\n'
-        return s
-
-
-class CibSource(object):
-    def __init__(self, json_dict={}):
-        pass
-
-
-class CIB2(object):
-    """ new version of the CIB
-    Internal representation of the CIB for testing"""
-
-    # We don't care about some of the CIB properties for matching. Define these special keys here
-    meta_keys = {'id', 'type', 'remote', 'local', 'filename', 'cib_source', 'timestamp', 'connection'}
-
-    def __init__(self, cib_dir=None):
-
-        # define three repository types for CIB sources
-        self.local = {}
-        self.connection = {}
-        self.remote = {}  # TODO rename remote to destination?
-
-        # concatenate all CIB repositories
-        self.entries = ChainMap(self.remote, self.connection, self.local)
-
-        if cib_dir:
-            self.load_cib(cib_dir)
-
-    def load_cib(self, cib_dir='cib/'):
-        """Read all CIB source files from directory CIB_DIR"""
-        for filename in os.listdir(cib_dir):
-            # TODO fix order of loading CIB sources
-            if filename.endswith(('.local', '.cib', '.connection')):
-                print('loading CIB source %s' % filename)
-                p = self.load_json(cib_dir + filename)
-                if not p:
-                    continue
-                p['filename'] = filename
-                properties = PropertyDict()
-                #properties.insert_dict(p.get('properties', {}))
-                import code
-                code.interact(local=locals(), banner='here')
-                self.register(p)
+    def register(self, cib_source):
+        self.idx[cib_source.idx] = cib_source
 
     def __getitem__(self, idx):
-        """Return a new PropertyDict containing the properties associated with a CIB index"""
-        entry = self.entries[idx]
-        properties = PropertyDict()
-
-        if not entry.get('type') == CONNECTION:
-            for k, v in entry.items():
-                if isinstance(v, NEATProperty):
-                    properties.insert(v)
-
-        else:
-            # for connection type entries expand the associated local and remote entries
-            local_id = entry.get('local')
-            remote_id = entry.get('remote')
-
-            new_entry = {}
-
-            # properties are overwritten in the following order: remote>connection>local
-            new_entry.update({i.key: i for i in self.entries[local_id].values() if isinstance(i, NEATProperty)})
-            new_entry.update({i.key: i for i in self.entries[idx].values() if isinstance(i, NEATProperty)})
-            new_entry.update({i.key: i for i in self.entries[remote_id].values() if isinstance(i, NEATProperty)})
-
-            for k, v in new_entry.items():
-                properties.insert(v)
-
-        return properties
-
-    def load_json(self, filename):
-        """Read JSON file"""
-
-        cib_file = open(filename, 'r')
-        try:
-            j = json.load(cib_file)
-        except json.decoder.JSONDecodeError as e:
-            logging.error("Could not parse CIB file " + filename)
-            print(e)
-            return
-        return j
-
-    def register(self, cib_source={}):
-        """Register loaded CIB sources with CIB repository"""
-        # CIB types [local] <<>> [connection] <<>> [remote]
-        current_type = cib_source.get("type")
-        if current_type not in ['local', 'connection', 'remote']:
-            logging.warning("Ignoring invalid type \"" + current_type + "\" for CIB " + cib_source['filename'])
-            return
-        current_idx = cib_source.get("id")
-        # cib_properties = PropertyDict()
-        # TODO for now we assume that all CIB properties are informational
-        # FIXME
-        # cib_properties.update(cib_source, precedence=NEATProperty.INFORMATIONAL)
-
-        # e.g. self.local['d1'] = {...}
-        getattr(self, current_type)[current_idx] = cib_source
-
-    def get_connection(self, idx):
-        """Return the full connection properties including the properties of the
-associated local and remote CIB entries.
-
-        """
-        connection = {}
-        connection.update(self.connection[idx])
-        remote_idx = connection[REMOTE]
-        local_idx = connection[LOCAL]
-        connection.update(self.local[local_idx])
-        connection.update(self.remote[remote_idx])
-        return {k: connection[k] for k in connection.keys() - CIB.meta_keys}
+        return self.idx[idx]
 
     def lookup(self, query, candidate_num=5):
-        """CIB lookup logic implementation. Appends a list of connection candidates to the query object. TODO
+        """
+        CIB lookup logic implementation. Appends a list of connection candidates to the query object. TODO
+
         """
         candidates = []
 
-        # check connection CIB sources first
-        for idx in self.connection.keys():
-            matched_properties = self[idx] & query.properties
-            candidate = NEATCandidate(self[idx])
-            skip_candidate = False
-            for property in matched_properties.values():
-                try:
-                    candidate.properties.insert(property)
-                except NEATPropertyError as e:
-                    logging.debug(e)
-                    skip_candidate = True
-                    break
-            if skip_candidate:
-                continue
-            candidates.append(candidate)
+    @property
+    def entries(self):
+        """
+        Returns a generator containing all expanded root CIB sources
 
-        candidates.sort(key=operator.attrgetter('score'), reverse=True)
-        query.candidates = candidates[0:candidate_num]
-        # TODO expand lookup to different cib types
+        """
+        for k, v in self.roots.items():
+            # expand all cib sources
+            for p in v.expand():
+                yield PropertyArray(*[p[k] for k in p.keys()])
 
-        for idx in self.local.keys():
-            matched_properties = self[idx] & query.properties
-            candidate = NEATCandidate(self[idx])
-            skip_candidate = False
-            for property in query.properties.values():
-                try:
-                    candidate.properties.insert(property)
-                except NEATPropertyError as e:
-                    logging.debug(e)
-                    skip_candidate = True
-                    break
-            if skip_candidate:
-                continue
-            candidates.append(candidate)
-        candidates.sort(key=operator.attrgetter('score'), reverse=True)
-        query.candidates = candidates[0:candidate_num]
+    def dump(self, show_all=False):
+        ts = shutil.get_terminal_size()
+        tcol = ts.columns
+        if show_all:
+            items = self.idx.items()
+        else:
+            items = self.roots.items()
 
-    def dump(self):
-        keys = list(self.entries.keys())
-        keys.sort()
-        for k in keys:
-            print('%s: %s' % (k, self[k]))
+        print("=" * int((tcol - 11) / 2) + " CIB START " + "=" * int((tcol - 11) / 2))
+        for k, v in items:
+            # expand all cib sources
+            for p in v.expand():
+                print('%s: %s' % (k, PropertyArray(*[p[k] for k in p.keys()])))
+        print("=" * int((tcol - 9) / 2) + " CIB END " + "=" * int((tcol - 9) / 2))
 
     def __repr__(self):
-        return 'CIB<%d>' % (len(self.local) + len(self.connection) + len(self.remote))
-
-    def __str__(self):
-        s = ''
-        for k, v in self.connection.items():
-            if not k == 'type':
-                s += str(v[LOCAL]) + ' <-> ' + str(k) + ' <-> ' + str(v[REMOTE]) + '\n'
-        return s
+        return 'CIB<%d>' % (len(cib.idx))
 
 
 if __name__ == "__main__":
+    cib = CIB('./cib/example2/')
+    b = cib['B']
+    c = cib['C']
+
+    for idx in cib.roots:
+        z = cib[idx].resolve_refs([])
+        print(z)
+
+    x = b.expand()
+
     import code
 
-    cib = CIB()
-    cib.load_cib()
-
-    print(cib)
-
-    query = NEATRequest({"is_wired_interface": "True"})
-    query.properties.insert(NEATProperty(('remote_address', '23:::23:12')))
-    query.properties.insert(NEATProperty(('foo', 'bar')))
-
-    code.interact(local=locals())
-    # create an example NEAT policy
-    p = NEATPolicy()
-    p.name = "dynamic"
-    p.match.insert(NEATProperty(("is_wired_interface", "True"), 2))
-    p.properties.insert(NEATProperty(("MTU", "9600"), 0))
-    p.properties.insert(NEATProperty(("TCP_CC", "cubic"), 2))
-
-    print("query: ")
-    print(query)
-
-    # lookup CIB
-    cib.lookup(query)
-
-    code.interact(local=locals())
-
-    cib = CIB2('/Users/bozakz/git/neat/policy/cib/example2/')
+    code.interact(local=locals(), banner='CIB')
